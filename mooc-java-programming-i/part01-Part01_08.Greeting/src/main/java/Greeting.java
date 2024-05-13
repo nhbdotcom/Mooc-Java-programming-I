de52fb4b0d8a975b4330e0fe751daf4be3c61c1b
Norman <nhb_jr@yahoo.com>
2024-05-13 23:47:08 +0800

    Complete 2 challenges

--- mooc-java-programming-i/part01-Part01_08.Greeting/src/main/java/Greeting.java
+++ mooc-java-programming-i/part01-Part01_08.Greeting/src/main/java/Greeting.java
@@ -6,10 +6,12 @@ public class Greeting {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String greet = "Hi ";
+        System.out.println("What's you name?");
         // User named input will be stored here
         String name = scanner.nextLine();
 
-        
+        System.out.println(greet + name);
 
     }
 }
