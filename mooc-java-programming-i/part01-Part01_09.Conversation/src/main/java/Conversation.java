4ded16d761695a9c839cb0334b65548e1da70a72
Norman <nhb_jr@yahoo.com>
2024-05-14 20:22:06 +0800

    "Challenge still in progress for part01-Part01_10.Story"

--- mooc-java-programming-i/part01-Part01_09.Conversation/src/main/java/Conversation.java
+++ mooc-java-programming-i/part01-Part01_09.Conversation/src/main/java/Conversation.java
@@ -5,8 +5,12 @@ public class Conversation {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Write your program here
-
+        String[] userInput = new String[2];
+        // The input of the user will be stored here
+        System.out.println("Greetings! How are you doing?");
+        userInput[0] = scanner.nextLine();
+        System.out.println("Oh, how interesting. Tell me more!");
+        userInput[1] = scanner.nextLine();
+        System.out.println("Thanks for sharing!");
     }
 }
--- mooc-java-programming-i/part01-Part01_10.Story/src/main/java/Story.java
+++ mooc-java-programming-i/part01-Part01_10.Story/src/main/java/Story.java
@@ -7,6 +7,33 @@ public class Story {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        String[] userInput = new String[2];
+        String[] storiesJourney = new String[]{
+            "I will tell you a story, but I need some information first.", 
+            "What is the main character called?",
+            "What is their job?",
+            "Here is the story:",
+            "Once upon a time there was Ada, who was a Data scientist.",
+            "On the way to work, Ada reflected on life.",
+            "Perhaps Ada will not be a Data scientist forever."
+        };
+        System.out.println(storiesJourney[0]);
+        System.out.println(storiesJourney[1]);
+        userInput[0] = scanner.nextLine();
+        System.out.println(storiesJourney[2]);
+        userInput[1] = scanner.nextLine();
 
+        System.out.println(storiesJourney[3]);
+
+        String newStory = storiesJourney[4].replaceFirst("Ada", userInput[0]);
+        newStory = newStory.replaceFirst("Data scientist", userInput[1]);
+        System.out.println(newStory);
+
+        String newStory1 = storiesJourney[5].replaceFirst("Ada", userInput[0]);
+        System.out.println(newStory1);
+
+        String newStory2 = storiesJourney[6].replaceFirst("Ada", userInput[0]);
+        newStory2 = newStory2.replaceFirst("Data scientist", userInput[1]);
+        System.out.println(newStory2);
     }
 }
