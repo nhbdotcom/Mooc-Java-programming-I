8d1cb63fa1590b37149ec59d4df944b3d90cf732
Norman <nhb_jr@yahoo.com>
2024-05-19 18:12:45 +0800

    Just solved the Story with basic Concatenation of variable and string

--- mooc-java-programming-i/part01-Part01_10.Story/src/main/java/Story.java
+++ mooc-java-programming-i/part01-Part01_10.Story/src/main/java/Story.java
@@ -1,39 +1,20 @@
-
 import java.util.Scanner;
-
+ 
 public class Story {
-
+ 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+ 
         // Write your program here
-        String[] userInput = new String[2];
-        String[] storiesJourney = new String[]{
-            "I will tell you a story, but I need some information first.", 
-            "What is the main character called?",
-            "What is their job?",
-            "Here is the story:",
-            "Once upon a time there was Ada, who was a Data scientist.",
-            "On the way to work, Ada reflected on life.",
-            "Perhaps Ada will not be a Data scientist forever."
-        };
-        System.out.println(storiesJourney[0]);
-        System.out.println(storiesJourney[1]);
-        userInput[0] = scanner.nextLine();
-        System.out.println(storiesJourney[2]);
-        userInput[1] = scanner.nextLine();
-
-        System.out.println(storiesJourney[3]);
-
-        String newStory = storiesJourney[4].replaceFirst("Ada", userInput[0]);
-        newStory = newStory.replaceFirst("Data scientist", userInput[1]);
-        System.out.println(newStory);
-
-        String newStory1 = storiesJourney[5].replaceFirst("Ada", userInput[0]);
-        System.out.println(newStory1);
-
-        String newStory2 = storiesJourney[6].replaceFirst("Ada", userInput[0]);
-        newStory2 = newStory2.replaceFirst("Data scientist", userInput[1]);
-        System.out.println(newStory2);
+        System.out.println("I will tell you a story, but I need some information first.");
+        System.out.println("What is the main character called?");
+        String name = scanner.nextLine();
+        System.out.println("What is their job?");
+        String job = scanner.nextLine();
+        System.out.println("Here is the story:");
+        System.out.println("Once upon a time there was " + name + ", who was " + job + ".");
+        System.out.println("On the way to work, " + name + " reflected on life.");
+        System.out.println("Perhaps " + name + " will not be " + job + " forever.");
+ 
     }
 }
